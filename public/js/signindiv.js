8f32686fff52e5209abfb5056cf70db1262cb7ba
JimmyFu <ppp456rrr@gmail.com>
2020-02-13 20:48:21 +0800

    google log in init

--- public/js/signindiv.js
+++ public/js/signindiv.js
@@ -170,3 +170,14 @@ function fblogin(){
 // FB.logout(function(response) {
 //     // Person is now logged out
 // });
+
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
